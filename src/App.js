39ae5dd93fd9e132187f71746164d01d0e1460a4
allaiar <radzhapov.allayar@gmail.com>
2023-06-27 16:45:39 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -9,67 +9,157 @@ function App() {
   const [email, setEmail] = useState("");
   const [img, setImg] = useState("");
   const [showInputs, setShowInputs] = useState(false);
+  const [permissions, setPermissions] = useState([]);
 
   const dispatch = useDispatch();
   const users = useSelector(selectUsers);
+
+  const PermissionChange = (e) => {
+    const value = e.target.value;
+    if (e.target.checked) {
+      setPermissions([...permissions, value]);
+    } else {
+      setPermissions(permissions.filter((item) => item !== value));
+    }
+  };
 
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
-  const AddUser = () => {
-    const userData = { name, email, img };
+  const AddUser = (e) => {
+    e.preventDefault();
+    const userData = {
+      name: name,
+      email: email,
+      image: img,
+      permissions: permissions,
+    };
     dispatch(addUser(userData));
+    setShowInputs(false);
     setName("");
     setEmail("");
-    setImg("");
+    setPermissions("");
   };
 
   return (
-    <div className="App border-2 border-bg-cyan-500 shadow-lg shadow-cyan-500/50 max-w-7xl mx-auto mt-20 rounded-3xl pb-3">
+    <div className="App border-2 border-bg-cyan-500 shadow-lg shadow-gray-500/50 max-w-7xl mx-auto mt-20 rounded-3xl pb-3">
       <div className="flex justify-between gap-x-10 mt-8 mb-5 border-b-2 pb-4 px-9">
         <h1 className="font-bold text-2xl text-gray-600">Команда</h1>
         {showInputs ? (
-          <div className="flex gap-x-10 mt-1">
-            <input
-              className="border border-gray-300 rounded-md py-2 px-4 mb-2 h-10"
-              type="text"
-              placeholder="Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              className="border border-gray-300 rounded-md py-2 px-4 mb-2 h-10"
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="border border-gray-300 rounded-md py-2 px-4 mb-4 h-10"
-              type="text"
-              placeholder="Image URL"
-              value={users.img}
-              onChange={(e) => setImg(e.target.value)}
-            />
+          <div className="">
+            <div className="flex gap-x-10">
+              <input
+                className="border border-gray-300 rounded-md py-2 px-4 mb-2 h-10"
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className="border border-gray-300 rounded-md py-2 px-4 mb-2 h-10"
+                type="text"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="border border-gray-300 rounded-md py-2 px-4 mb-4 h-10"
+                type="text"
+                placeholder="Image URL"
+                value={users.img}
+                onChange={(e) => setImg(e.target.value)}
+              />
+              <svg
+                onClick={() => setShowInputs(false)}
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-8 w-8 text-gray-600 mt-2 cursor-pointer"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </div>
+            <div className="flex gap-x-5">
+              <label>
+                <input
+                  type="checkbox"
+                  value="Модерация объявлений"
+                  checked={permissions.includes("Модерация объявлений")}
+                  onChange={PermissionChange}
+                />
+                Модерация объявлений{" "}
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Блог"
+                  checked={permissions.includes("Блог")}
+                  onChange={PermissionChange}
+                />
+                Блог
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Тех. поддержка"
+                  checked={permissions.includes("Тех. поддержка")}
+                  onChange={PermissionChange}
+                />
+                Тех. поддержка
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Обращения клиентов"
+                  checked={permissions.includes("Обращения клиентов")}
+                  onChange={PermissionChange}
+                />
+                Обращения клиентов
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Аналитика"
+                  checked={permissions.includes("Аналитика")}
+                  onChange={PermissionChange}
+                />
+                Аналитика
+              </label>
+              <label>
+                <input
+                  type="checkbox"
+                  value="Акции"
+                  checked={permissions.includes("Акции")}
+                  onChange={PermissionChange}
+                />
+                Акции
+              </label>
+            </div>
           </div>
         ) : (
           <svg
-          style={{marginLeft:"760px",position:"relative" }}
-          onClick={() => setShowInputs(true)}
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8 text-gray-600 mt-2 cursor-pointer"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
+            style={{ marginLeft: "760px" }}
+            onClick={() => setShowInputs(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-gray-600 mt-2 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
         )}
         <button
           className="bg-green-500 text-white w-56 h-12 rounded-2xl hover:bg-green-600 font-bold"
@@ -80,7 +170,7 @@ function App() {
       </div>
       <div className="mx-auto px-3">
         {users.map((el) => (
-          <UserItem key={el.id} user={el} {...el}/>
+          <UserItem key={el.id} user={el} {...el} />
         ))}
       </div>
     </div>
--- src/components/UserItem/index.jsx
+++ src/components/UserItem/index.jsx
@@ -10,7 +10,9 @@ const UserItem = ({ user }) => {
   const [img, setImg] = useState(user.image);
   const [showMenu, setShowMenu] = useState(false);
   const [showMenuToggle, setShowMenuToggle] = useState(true);
-  const [ava] = useState("https://cdn1.flamp.ru/07e1cd43a9e5be02947aa62e9bc48688.jpg")
+  const [ava] = useState(
+    "https://cdn1.flamp.ru/07e1cd43a9e5be02947aa62e9bc48688.jpg"
+  );
 
   const Delete = () => {
     dispatch(deleteUser(user.email));
@@ -24,7 +26,7 @@ const UserItem = ({ user }) => {
   const MenuTimes = (name) => {
     setShowMenu(name === showMenu ? null : name);
     setShowMenuToggle(true);
-  }
+  };
 
   const Edit = () => {
     if (editing) {
@@ -50,37 +52,40 @@ const UserItem = ({ user }) => {
     <div className="flex items-center mb-4 justify-between">
       <div className="flex">
         <div className="w-20 h-20 mr-4 ml-4">
-        <div className="w-20 h-20 mr-4 ml-4">
-          {!img ? (
-            <img
-              className="rounded-full w-full h-full object-cover"
-              src={ava}
-              alt="img"
-            />
-          ) : (
-            <img
-              className="rounded-full w-full h-full object-cover"
-              src={img}
-              alt="img"
-            />
-          )}
-        </div>
+          <div className="w-20 h-20 mr-4 ml-4">
+            {!img ? (
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={ava}
+                alt="img"
+              />
+            ) : (
+              <img
+                className="rounded-full w-full h-full object-cover"
+                src={img}
+                alt="img"
+              />
+            )}
+          </div>
         </div>
         {!editing ? (
           <div>
             <div className="flex gap-x-5">
-              <h2 className="text-xl font-bold text-gray-600">{user.name}</h2>
-              <h3 className="text-gray-500 font-medium">{user.email}</h3>
+              <h2 className="text-xl font-bold text-gray-600 ml-5">
+                {user.name}
+              </h2>
+              <h3 className="text-gray-500 font-medium ml-2">{user.email}</h3>
             </div>
-            <div className="ml-auto flex gap-x-1">
-            {user.permissions && user.permissions.map((permission) => (
-                <div
-                  className="border p-1 rounded-2xl text-gray-500 px-3 pb-1.5"
-                  key={permission}
-                >
-                  {permission}
-                </div>
-              ))}
+            <div className="ml-4 mt-2 flex gap-x-1">
+              {user.permissions &&
+                user.permissions.map((permission) => (
+                  <div
+                    className="border p-1 rounded-2xl text-gray-500 px-3 pb-1.5"
+                    key={permission}
+                  >
+                    {permission}
+                  </div>
+                ))}
             </div>
           </div>
         ) : (
@@ -132,26 +137,32 @@ const UserItem = ({ user }) => {
               <button
                 className="ml-4 px-4 py-2 rounded bg-red-500 text-white"
                 onClick={Delete}
-                >
+              >
                 &times;
               </button>
               <button
                 className="ml-2 px-4 py-2 rounded bg-blue-500 text-white mr-3"
                 onClick={Edit}
-                >
+              >
                 Edit
               </button>
             </>
           )}
-      {showMenuToggle ? (
-        <button className="mr-5 text-4xl" onClick={() => MenuToggle(name)}>
-          ...
-        </button>
-      ) : (
-        <button className="mr-4 text-4xl" onClick={() => MenuTimes(name)}>
-          &times;
-        </button>
-      )}
+          {showMenuToggle ? (
+            <button
+              className="mr-5 text-4xl mb-10"
+              onClick={() => MenuToggle(name)}
+            >
+              ...
+            </button>
+          ) : (
+            <button
+              className="mr-4 text-4xl mb-7"
+              onClick={() => MenuTimes(name)}
+            >
+              &times;
+            </button>
+          )}
         </div>
       )}
     </div>
